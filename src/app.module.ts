233ad5cc4a121bd6563675d2fd4a5b02439ce6b0
Xaviniesta <88670766+Xaviniestalex@users.noreply.github.com>
2022-12-02 10:43:47 +0800

    Set up psql database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { env } from './../env';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -8,10 +11,23 @@ import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.database.host,
+      port: env.database.port,
+      username: env.database.user,
+      password: env.database.password,
+      database: env.database.database,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: env.nodeEnv !== 'production', // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+    }),
     AuthModule,
     UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userService.findOne(username);
+    const user = await this.userService.findOneByUsername(username);
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
